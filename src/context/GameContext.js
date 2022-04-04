01dae98140b2e855e276471ef00840891e21ba14
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-04 15:44:35 -0700

    moved pass card and findcardindex to executePassButton

---
+++ src/context/GameContext.js
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState } from 'react';
+import initialCards from '../cards-data';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+  const gameState = {
+    selectedCard,
+    setSelectedCard,
+    deck,
+    setDeck,
+    playerOneHand,
+    setPlayerOneHand,
+    playerTwoHand,
+    setPlayerTwoHand,
+    playerThreeHand,
+    setPlayerThreeHand,
+    from,
+    setFrom,
+    to,
+    setTo,
+  };
+  return <GameContext.Provider value={gameState}>{children}</GameContext.Provider>;
+};
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+
+  return context;
+};
+
+export { useGameContext, GameProvider };
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserProvider } from './context/UserContext';
+import { GameProvider } from './context/GameContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider>
+    <GameProvider>
       <App />
-    </UserProvider>
+    </GameProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
